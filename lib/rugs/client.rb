3cabecded6bdd3897bee2962e2b84297158b4319
Mike Bethany <mikbe.tk@gmail.com>
2011-09-24 21:42:03 -0400

    Added executable and commandable

--- lib/rugs/client.rb
+++ lib/rugs/client.rb
@@ -1,17 +1,23 @@
+requie 'commandable'
+
 module RUGS
 
   class Client
-  
+    extend Commandable
+    
+    command 'stored remotes'
     def remote_list
       @remote_list ||= Config.load("remotes")
     end
  
+    command 'remotes that will automatically be added to repos'
     def default_remotes
       @default_remotes ||= remote_list.select do |remote, keys|
         keys[:default]
       end
     end
     
+    command 'make and set up a remote'
     def create(repo_name, *remote)
       
       make_local_repo(repo_name)
@@ -25,6 +31,7 @@ module RUGS
       
     end
     
+    command 'add a remote server to be used when setting up repos'
     def remote_add(remote, url_path, default=false)
       url, path = url_path.split(":")
       
@@ -34,15 +41,18 @@ module RUGS
       Config.save("remotes", remote_list)
     end
 
+    command 'remove a remote server'
     def remote_remove(remote)
       remote_list.delete(remote)
       Config.save("remotes", remote_list)
     end
 
+    command 'make an existing remote a default remote'
     def default(remote, is_default=true)
       remote_list[remote][:default] = is_default
     end
 
+    command 'remove a remote from the default list'
     def undefault(remote)
       default remote, false
     end
